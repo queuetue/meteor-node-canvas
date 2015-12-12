55ba85fbe97842284d8e401893f26d1be1f5cde1
Scott Russell <queuetue@gmail.com>
2015-12-12 10:38:12 -0500

    meta work

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'queuetue:node-canvas',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Meteor Package for Server Side Node Canvas',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/queuetue/meteor-node-canvas',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Npm.depends({
-  'canvas': '1.1.6'
+  'canvas': '1.3.5'
 });
 
 Package.onUse(function(api) {
